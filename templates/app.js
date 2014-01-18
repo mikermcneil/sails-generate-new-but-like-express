c7b82ecf960931f5e28a2b5da828232051a7e5f7
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-18 00:39:30 -0600

    Pushed updated views

--- templates/app.js
+++ templates/app.js
@@ -1,5 +1,23 @@
+/**
+ * Module dependencies
+ */
 var Sails = require('sails/lib/app');
 var sails = new Sails();
 
 
-sails.lift();
+
+// Load logic from `routes` directory
+var routes = require('./routes');
+var user = require('./routes/user');
+
+
+// Lift server
+sails.lift({
+	views: {
+		engine: 'jade'
+	},
+	routes: {
+		'get /': routes.index,
+		'get /users': user.list
+	}
+});
